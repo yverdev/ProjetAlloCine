45900ac8cb59a0e396f1ab25a9f1914cee43a0d3
yverdev <contact@yverdev.com>
2020-03-31 22:21:27 +0200

    update index.html and main.js

--- JS/main.js
+++ JS/main.js
@@ -1,58 +1,74 @@
+/*
+	* AU CHARGEMENT DE LA PAGE ON AFFICHE LA LISTE DES FILMS DEPUIS Le ARRAY movies (dans js/data.js)
+*/
+// Selection de la liste des films
+var list = $('.list');
+console.log('list');
+// On cache la div.details au chargement de la page
+$('div.details').hide(); //where is the div.details?
+// On affiche le nombre de films
+$('.supertitle span').text(movies.length) //where the class?
 
-	/* AU CHARGEMENT DE LA PAGE ON AFFICHE LA LISTE DES FILMS DEPUIS Le ARRAY movies (dans js/data.js) */
-    // Selection de la liste des films
-    var list = $('.list');
-    console.log('list');
-    // On cache la div.details au chargement de la page
-    $('div.details').hide(); //where is the div.details?
-    // On affiche le nombre de films
-    $('.supertitle span').text(movies.length) //where the class?
-    
-    // On génère les items des films dans la div.list
-    for (var i = 0; i < movies.length; i++) {
-        list.append(`
-                <div class="col-12 col-md-4 mb-5 item text-center py-2 px-4">
-    
-                    <div data-id="${movies[i].id}" class="card">
-                        <img class="card-img-top" src="${movies[i].image}" alt="Card image cap">
-    
-                        <div class="card-info">
-                        <h2>${movies[i].title}</h2>
-                        <p>${movies[i].date}</p>
-                        <p>${movies[i].genre}</p>
-                        <p>${movies[i].director}</p>
-                        <p>${movies[i].duration}</p>
-    
-                        </div> 
-                    </div>     
-                </div>
-                `)
-            }
-    
-    function getDetail() {
-        window.location.href = 'fiche-films.html?id=' + $(this).data('id');
-    }
-    
-    $('div.card').on('click', getDetail);
-    
-    /* $('div.card').hover(function(){
-        $(this).slideUp(800);
+// On génère les items des films dans la div.list
+for (var i = 0; i < movies.length; i++) {
+    list.append(`
+			<div class="col-12 col-md-4 mb-5 item text-center py-2 px-4">
+
+				<div data-id="${movies[i].id}" class="card">
+                    <img class="card-img-top" src="${movies[i].image}" alt="Card image cap">
+
+                    <div class="card-info">
+                    <h2>${movies[i].title}</h2>
+                    <p>${movies[i].date}</p>
+                    <p>${movies[i].genre}</p>
+                    <p>${movies[i].director}</p>
+                    <p>${movies[i].duration}</p>
+
+                    </div> 
+                    
+                      
+                    
+                    </div>
+                   
+			</div>
+		`)
+}
+
+
+
+function getDetail() {
+    window.location.href = 'fiche-films.html?id=' + $(this).data('id');
+}
+
+$('div.card').on('click', getDetail);
+
+
+
+
+/* $('div.card').hover(function(){
+    $(this).slideUp(800);
     //$(this).slideDown(800);
-    }); */
-    $('div.card-info').hide();
-    //var cardInfoHide = $('div.card-info').hide();
-    //var cardInfoShow = $('div.card-info').show();
+}); */
 
-    $('div.card').hover(function(){
-    
-    if($('card-img-top').hover()){
-        var imgUp = $(this).slideUp(800);
-        /* $('div.card-info').show().slideDown(800); */
-        if(imgUp==slideUp()){
-            $('div.card')
-        }   
-    };
+// $('div.card-info').hide();
+
+
+//var cardInfoHide = $('div.card-info').hide();
+//var cardInfoShow = $('div.card-info').show();
+
+$('div.card').hover(function () {
+
+
+    // if($('card-img-top').hover()){
+    //     var imgUp = $(this).slideUp(800);
+    //     /* $('div.card-info').show().slideDown(800); */
+    //     if(imgUp==slideUp()){
+    //         $('div.card')
+    //     }
+
+
+    // };
     /* if(imgUp==0)
-        }
+    }
      */
 });
