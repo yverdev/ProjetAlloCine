c9611d26ceea23ffa904c73e51594c9674fa9c44
yverdev <contact@yverdev.com>
2020-04-11 21:35:15 +0200

    update css and html

--- JS/search.js
+++ JS/search.js
@@ -2,14 +2,15 @@
     $('#searchInputBar').keyup(function(){
         var search = $(this).val();
         var data = 'keyword' + search;
+        moviesToPrint = movies.filter(function (el) { return el.nearly == true && el.nearl == false})
         var respons = $.each([moviesData].title, function(e){
-            $("#h").append("Titre" + e + '<br>');
             console.log(respons);
+            $("#h").append("Titre" + e + '<br>');
+            
         });
         if (search.lenght>1){
             $('#h').html(respons).show();      
-        }else{
+         }else{
             $('#h').html('Aucun résultat pour cette recherche !').show();
         }
      });  
-
