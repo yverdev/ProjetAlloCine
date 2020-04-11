34965bdd06cba143f03c195e24c708d6e70d30c1
yverdev <contact@yverdev.com>
2020-04-11 21:43:11 +0200

    final adjustement result search bar

--- JS/search.js
+++ JS/search.js
@@ -2,13 +2,12 @@
     $('#searchInputBar').keyup(function(){
         var search = $(this).val();
         var data = 'keyword' + search;
-        moviesToPrint = movies.filter(function (el) { return el.nearly == true && el.nearl == false})
         var respons = $.each([moviesData].title, function(e){
             console.log(respons);
             $("#h").append("Titre" + e + '<br>');
             
         });
-        if (search.lenght>1){
+        if (search.lenght>4){
             $('#h').html(respons).show();      
          }else{
             $('#h').html('Aucun résultat pour cette recherche !').show();
