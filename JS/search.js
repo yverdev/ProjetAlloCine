2c3466e5f13f401c122e3bd8f451079487b9a2f2
yverdev <contact@yverdev.com>
2020-04-11 15:08:51 +0200

    first part work of the search bar

--- JS/search.js
+++ JS/search.js
@@ -1,2 +1,13 @@
 
-   
+    $('#searchInputBar').keyup(function(){
+        var search = $(this).val();
+        var data = 'keyword' + search;
+        var respons = movies;
+        console.log(respons);
+        if (search.lenght>1){
+            $('#h').html(respons).show();      
+        }else{
+            $('#h').html('Aucun résultat pour cette recherche !').show();
+        }
+    });  
+
